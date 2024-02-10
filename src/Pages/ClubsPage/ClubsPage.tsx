269bf4a3880aebd26d46bcc46a878fa49fe2ba36
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-02-10 16:55:46 +0100

    filtering clubs by league

--- src/Pages/ClubsPage/ClubsPage.tsx
+++ src/Pages/ClubsPage/ClubsPage.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 import ClubList from "../../Components/ClubsList/ClubList";
 import axios from "axios";
+import { League } from "../../Helpers/EnumTypes";
 
 type Props = {};
+
+const options = Object.values(League).filter(
+  (value) => isNaN(Number(value)) === true
+);
 
 const ClubsPage = (props: Props) => {
   const [clubs, setClubs] = useState<any[]>([]);
@@ -15,9 +20,31 @@ const ClubsPage = (props: Props) => {
     };
     getClubsInit();
   }, []);
+  const onChangeLeague = async (e: any) => {
+    let league: string = e.target.value;
+    if (!league.startsWith("All")) {
+      league = League[e.target.value];
+      console.log(league);
+      const filteredClubs = await axios.get<any[]>(
+        `https://localhost:7019/api/club?league=${league}`
+      );
+      setClubs(filteredClubs.data);
+    } else {
+      const filteredClubs = await axios.get<any[]>(
+        `https://localhost:7019/api/club`
+      );
+      setClubs(filteredClubs.data);
+    }
+  };
   return (
     <div>
       ClubsPage
+      <select onChange={onChangeLeague}>
+        <option>All Leagues</option>
+        {options.map((option, index) => {
+          return <option key={index}>{option}</option>;
+        })}
+      </select>
       <ClubList clubs={clubs} />
     </div>
   );
